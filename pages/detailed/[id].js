9ade4b92af41afc8f12d108a3273bbf01c256cd3
Andy Keogh <Andykeoghleic@gmail.com>
2022-06-04 17:34:20 +1000

    ADD: new page. for detailed coins desc

---
+++ pages/detailed/[id].js
@@ -0,0 +1,61 @@
+import React from "react";
+import MarketDataChart from "../../components/Charts/MarketDataChart";
+import CoinDetailed from "../../components/UI/CoinDetailed";
+
+const CoinCharting = ({ coinData, chartDataJson }) => {
+  const { market_caps, prices, total_volumes } = coinData;
+  const { developer_data, community_data } = chartDataJson;
+
+  return (
+    <>
+      <div className=" flex bg-companyBranding  flex-col md:flex-row ">
+        <MarketDataChart
+          marketCap={market_caps}
+          prices={prices}
+          total_volumes={total_volumes}
+        />
+        <div className="flex flex-col max-h-screen">
+          <CoinDetailed coinMarketInfo={chartDataJson} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CoinCharting;
+
+//NextJs needs adding even though its not used.
+export const getStaticPaths = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+
+  try {
+    const [chartData, marketData] = await Promise.all([
+      fetch(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=30&interval=daily`
+      ),
+      fetch(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=true&community_data=true
+      `),
+    ]);
+
+    const [chartDataJson, marketDataJson] = await Promise.all([
+      chartData.json(),
+      marketData.json(),
+    ]);
+
+    if (!chartDataJson || !marketDataJson) {
+      return { notFound: true };
+    }
+    return {
+      props: { coinData: chartDataJson, chartDataJson: marketDataJson },
+    };
+  } catch (error) {
+    return { notFound: true };
+  }
+}
